224522d3f62a64b9b1c35e4edabdc0020d656292
Jones Melton <jones@reachcreative.com>
2017-04-18 15:42:38 -0500

    Fix syntax errors

--- booper.rb
+++ booper.rb
@@ -5,44 +5,34 @@ mq = Rabbit.new
 puts "listening"
 mq.subscribe do |sound_type|
   case sound_type
-    when "foghorn"
-      `omxplayer ./sounds/foghorn.mp3`
+  when "foghorn"
+    `omxplayer ./sounds/foghorn.mp3`
 
-    when "scream"
-      `omxplayer ./sounds/scream.mp3`
-  end
-  
+  when "scream"
+    `omxplayer ./sounds/scream.mp3`
+
   when "apparently"
-      `omxplayer ./sounds/apparently.mp3`
-  end
+    `omxplayer ./sounds/apparently.mp3`
 
   when "brat"
-      `omxplayer ./sounds/baby.mp3`
-  end
+    `omxplayer ./sounds/baby.mp3`
 
   when "birds"
-      `omxplayer ./sounds/birds.mp3`
-  end
+    `omxplayer ./sounds/birds.mp3`
 
   when "powerball"
-      `omxplayer ./sounds/powerball.mp3`
-  end
+    `omxplayer ./sounds/powerball.mp3`
 
   when "psycho"
-      `omxplayer ./sounds/psycho.mp3`
-  end
+    `omxplayer ./sounds/psycho.mp3`
 
   when "brat"
-      `omxplayer ./sounds/brat.mp3`
-  end
+    `omxplayer ./sounds/brat.mp3`
 
   when "ting"
-      `omxplayer ./sounds/ting.mp3`
-  end
+    `omxplayer ./sounds/ting.mp3`
 
   when "great"
-      `omxplayer ./sounds/great.mp3`
+    `omxplayer ./sounds/great.mp3`
+
   end
-
-end
-
