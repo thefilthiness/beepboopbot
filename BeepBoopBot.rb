22ec3c063db2b966bf7b98384b812a7ce181b50a
Jones Melton <jones@reachcreative.com>
2017-04-18 20:28:12 -0500

    Install dependencies for testing

--- BeepBoopBot.rb
+++ BeepBoopBot.rb
@@ -6,7 +6,7 @@ Dotenv.load
 
 class BeepBoopBot < SlackRubyBot::Bot
   mq = Rabbit.new
-  
+
   scan(/lunch|foghorn|ironsides/i) do |client, data, match|
     mq.push("foghorn")
   end
@@ -14,33 +14,33 @@ class BeepBoopBot < SlackRubyBot::Bot
   scan(/pickle|tickle/i) do |client, data, match|
     mq.push("scream")
   end
-  
+
     scan(/apparently|alan/i) do |client, data, match|
     mq.push("apparently")
   end
-  
+
     scan(/brat/i) do |client, data, match|
     mq.push("brat")
   end
-  
+
     scan(/birds/i) do |client, data, match|
     mq.push("birds")
   end
-  
+
     scan(/powerball/i) do |client, data, match|
     mq.push("powerball")
   end
-  
+
     scan(/scary|stacey|juju/i) do |client, data, match|
     mq.push("psycho")
   end
-  
+
     scan(/pheasant|glass/i) do |client, data, match|
     mq.push("ting")
   end
-  
+
     scan(/great/i) do |client, data, match|
     mq.push("great")
   end
-  
+
 end
--- Gemfile
+++ Gemfile
@@ -4,5 +4,15 @@ gem 'bunny'
 gem 'celluloid-io'
 gem 'dotenv'
 gem 'foreman'
-gem 'pry'
 gem 'slack-ruby-bot'
+
+group :development, :test do
+  gem 'bunny-mock'
+  gem 'rspec'
+  gem 'vcr'
+  gem 'webmock'
+end
+
+group :development do
+  gem 'pry'
+end
